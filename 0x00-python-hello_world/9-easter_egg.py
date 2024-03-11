8f2755ac055d3f2b84136a2697a47bccb3032f85
Gideon-Yebei <gideon-yebei@proton.me>
2024-03-11 20:03:08 +0300

    easter egg

---
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python3
+# The `this` module is a bit of an easter egg in Python. When imported, it doesn't provide any functions, classes, or variables that you can use in your code. Instead, it automatically prints a poem to the console. The poem is "The Zen of Python", by Tim Peters, which outlines some of the guiding principles behind the design of the Python language. It's a bit of fun, but also a useful reminder of what Python is all about.
+import this
