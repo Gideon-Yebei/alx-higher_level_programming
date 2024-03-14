b2de9f5cd872dd822f3aa7f52aa29ee3ce6a83f6
Gideon-Yebei <gideon-yebei@proton.me>
2024-03-14 23:09:13 +0300

    Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    argc = len(sys.argv)
-    if argc == 1:
-        print("0")
-    else:
-        sum = 0
-        for i in range(1, argc):
-            sum += int(sys.argv[i])
-        print("{}".format(sum))
-
+  import sys
+  argc = len(sys.argv)
+  if argc == 1:
+    print("0")
+  else:
+    sum = 0
+    for i in range(1, argc):
+      sum += int(sys.argv[i], base=10)
+    print("{}".format(sum))
