28de3049d2d3efdcc0c85c75e2114b45d74e1aa0
Gideon-Yebei <gideon-yebei@proton.me>
2024-03-18 22:50:23 +0300

    Switch

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
-def switch(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    new_list = my_list.copy()
-    new_list[0] = my_list[-1]
-    new_list[-1] = my_list[0]
-    return new_list
+a = 89
+b = 10
+a, b = b, a
+print("a={:d} - b={:d}".format(a, b))
